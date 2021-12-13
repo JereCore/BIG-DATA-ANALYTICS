1838f6e8986a6850eda078e878bba48d8342bff5
JereCore <64321243+JereCore@users.noreply.github.com>
2021-12-13 14:08:03 -0500

    Update Validando_pagos.R

--- Validando_pagos.R
+++ Validando_pagos.R
@@ -1,17 +1,30 @@
-octubre$monto_2 <- sub(".", "", octubre$`Monto de pago de Tarjeta`, fixed = TRUE)
-octubre$monto_3 <- sub(",", ".", octubre$monto_2, fixed = TRUE)
-octubre$monto_4 <- as.numeric(octubre$monto_3)
-octubre$monto <- octubre$monto_4
-octubre$monto_2 <- NULL
-octubre$monto_3 <- NULL
-octubre$monto_4 <- NULL
+#CARGANDO DATOS
+
+library(readr)
+Noviembre <- read_delim("JLP/PAGOS_TC/Noviembre.txt", 
+                        "\t", escape_double = FALSE, col_types = cols(Monto_de_pago_de_Tarjeta = col_character()), 
+                        trim_ws = TRUE)
+View(Noviembre)
+
+#TRANSFORMANDO DE , DECIMAL A . PUNTO DECIMAL Y LUEGO A NÚMERICO
+Noviembre$monto_2 <- sub(".", "", Noviembre$`Monto de pago de Tarjeta`, fixed = TRUE)
+Noviembre$monto_3 <- sub(",", ".", Noviembre$monto_2, fixed = TRUE)
+Noviembre$monto_4 <- as.numeric(Noviembre$monto_3)
+Noviembre$monto <- Noviembre$monto_4
+Noviembre$monto_2 <- NULL
+Noviembre$monto_3 <- NULL
+Noviembre$monto_4 <- NULL
 
 
-class(octubre$`Monto de pago de Tarjeta`)
+#VALIDANDO TIPO DE DATO
+class(Noviembre$`Monto de pago de Tarjeta`)
+class(Noviembre$monto)
 
-octubre_agrupado <- sqldf("Select [Número Identificación], SUM(monto) from octubre group by [Número Identificación] ")
+#AGRUPANDO MONTOS POR CLIENTE
+Noviembre_agrupado <- sqldf("Select [Número Identificación], SUM(monto) from Noviembre group by [Número Identificación] ")
 
-X <- octubre_agrupado$`SUM(monto)`
+#ASIGNANDO MONTO A VARIABLE X PARA SU EVALUACIÓN DE CORTES
+X <- Noviembre_agrupado$`SUM(monto)`
 
 res1 <- hist(X)
 
@@ -25,10 +38,10 @@ mat
 
 setwd("C:/Users/jloayzap/Documents/JLP/PAGOS_TC")
 
+#OBTENIENDO MONTOS MAYORES A 30000 SOLES POR PAGO Y LUEGO TRANSFORMANDOLO A DOLAR
 options(digits =9) # <- validar digitos decimales
-octubre_30000 <- sqldf("select * from octubre where monto >= 30000")
+Noviembre_30000 <- sqldf("select * from Noviembre where monto >= 30000")
 
+Noviembre_30000$monto_dolar <- Noviembre_30000$monto/4.024419355
 
-octubre_30000$monto_dolar <- octubre_30000$monto/4.024419355
-
-write.csv(octubre_30000,"octubre_30000.csv")
+write.csv(Noviembre_30000,"Noviembre_30000.csv")
